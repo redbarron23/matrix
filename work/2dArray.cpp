a2ac131c2fa6804d3d77dc97c7c242f008332261
james hourihane <james.hourihane@cloudtp.com>
2017-10-10 19:54:23 -0700

    second commit

--- work/2dArray.cpp
+++ work/2dArray.cpp
@@ -2,28 +2,22 @@
 
 #include <iostream>
 
-int** create2DArray(unsigned row, unsigned col)
-{
+int** create2DArray(unsigned row, unsigned col) {
      int** array2D = 0;
      array2D = new int*[row];
 
-     for (int h = 0; h < row; h++)
-     {
-          array2D[h] = new int[col];
+     for (int i = 0; i < row; i++) {
+          array2D[i] = new int[col];
 
-          for (int w = 0; w < col; w++)
-          {
-               // fill in some initial values
-               // (filling in zeros would be more logic, but this is just for the example)
-               array2D[h][w] = w + col * h;
+          for (int j = 0; j < col; j++) {
+               array2D[i][j] = j + col * i;
           }
      }
 
      return array2D;
 }
 
-int main ()
-{
+int main () {
 
     printf("Creating a 2D array2D\n");
     printf("\n");
@@ -36,10 +30,8 @@ int main ()
     // print contents of the array2D
     printf("Array contents: \n");
 
-    for (int h = 0; h < row; h++)
-    {
-         for (int w = 0; w < col; w++)
-         {
+    for (int h = 0; h < row; h++) {
+         for (int w = 0; w < col; w++) {
               printf("%i,", my2DArray[h][w]);
          }
          printf("\n");
